56b5d1a148b7bc08ac219a14f6d9c339c986636a
David Arnold <david.arnold@sihealth.co.uk>
2022-07-30 13:06:13 +0100

    Updated to react-router-dom@6 and fixed ReactDOM.render() and Routes deprecated warnings, and added scss-tokenizer@0.4.2 to fix high severity warning in git repo on github

--- app/src/Nav.tsx
+++ app/src/Nav.tsx
@@ -1,13 +1,13 @@
 // @ts-ignore
 import React from "react";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import HOME_COM from './components/Home';
 import EDUCATION_COM from './components/Education';
 import COMPUTING_COM from './components/Computing';
 import CONTACT_COM from './components/Contact';
 
 const BasicExample = () => (
-    <Router>
+    <BrowserRouter>
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
@@ -16,11 +16,13 @@ const BasicExample = () => (
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
-        <Route exact path="/" component={Home}/>
-        <Route path="/education" component={Education}/>
-        <Route path="/computing" component={Computing}/>
-        <Route path="/contact" component={Contact}/>
-    </Router>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/education" element={<Education />}/>
+            <Route path="/computing" element={<Computing />}/>
+            <Route path="/contact" element={<Contact />}/>
+        </Routes>
+    </BrowserRouter>
 );
 
 const Home = () => (
